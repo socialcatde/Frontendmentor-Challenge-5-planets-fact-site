6c7d1295ff0d496ce58d3f20e9309e3545446bdd
Tom Rosenkranz <tom@socialcat.de>
2021-09-07 20:00:47 +0200

    first day chaos

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = (config) => {
   config.addPassthroughCopy("img");
   config.addPassthroughCopy("css");
   config.addPassthroughCopy("js");
+
   return {
     passthroughFileCopy: true,
   };
---
+++ js/app.js
@@ -0,0 +1,6 @@
+const buttonSelect = document.querySelector("#Mercury");
+const overviewContent = document.querySelector("#overview");
+
+buttonSelect.addEventListener("click", function () {
+  overviewContent.innerHTML = "LOLO";
+});
