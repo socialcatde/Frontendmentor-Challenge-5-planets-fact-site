1be9880cd86194cf95feb85ebfb2398fd73f51dd
Tom Rosenkranz <tom@socialcat.de>
2021-09-07 16:27:55 +0200

    Setting Up Still

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+module.exports = (config) => {
+  config.addPassthroughCopy("img");
+  config.addPassthroughCopy("css");
+  config.addPassthroughCopy("js");
+  return {
+    passthroughFileCopy: true,
+  };
+};
