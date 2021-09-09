1c1ff69915715317885f5ee96b7cad5225e06b27
Tom Rosenkranz <tom@socialcat.de>
2021-09-09 21:44:20 +0200

    Final changes for today

--- js/app.js
+++ js/app.js
@@ -4,27 +4,3 @@ hamburger.addEventListener("click", function () {
   document.body.classList.toggle("nav_list_open_body");
   hamburger.classList.toggle("is-active");
 });
-
-/* Farbauswahl für borders */
-
-const contentButton = document.querySelector(".contentButton");
-const planetCurrent = document.querySelector("h3").innerText.toLowerCase();
-let hoverColor = {
-  mercury: "419EBB",
-  venus: "EDA249",
-  earth: "6D2ED5",
-  mars: "D14C32",
-  jupiter: "D83A34",
-  saturn: "CD5120",
-  uranus: "1EC1A2",
-  neptune: "2D68F0",
-};
-let response = "";
-
-if (planetCurrent in hoverColor) {
-  response = hoverColor[`${planetCurrent}`];
-}
-
-contentButton.addEventListener("mouseover", function () {
-  contentButton.style.color = "blue";
-});
