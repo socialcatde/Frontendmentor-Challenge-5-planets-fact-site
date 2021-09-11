5bebfc21845b70b358cdd17c98da9e2987d5314d
Tom Rosenkranz <tom@socialcat.de>
2021-09-11 14:16:32 +0200

    Refactoring some code

--- js/app.js
+++ js/app.js
@@ -1,25 +1,55 @@
 const hamburger = document.querySelector(".hamburger--squeeze");
 const contentButton = document.querySelectorAll(".contentButton");
-const outsidePlanet = document.querySelector(".planet-box .outside-planet");
-const insidePlanet = document.querySelector(".planet-box .inside-planet");
-const realPlanet = document.querySelector(".planet-box .real-planet");
-const TxtOutsidePlanet = document.querySelector(".information .outside-txt");
-const TxtInsidePlanet = document.querySelector(".information .inside-txt");
-const TxtRealPlanet = document.querySelector(".information .real-txt");
+
+const planetFacts = [
+  {
+    planet: ".planet-box .outside-planet",
+    textPlanet: ".information .outside-txt",
+    buttonPlanet: ".overview",
+  },
+  {
+    planet: ".planet-box .inside-planet",
+    textPlanet: ".information .inside-txt",
+    buttonPlanet: ".structure",
+  },
+  {
+    planet: ".planet-box .real-planet",
+    textPlanet: ".information .real-txt",
+    buttonPlanet: ".surface",
+  },
+];
 
 const subMenuId = document.querySelector(".submenu").id;
-const overviewButton = document.querySelector(".overview");
-const structureButton = document.querySelector(".structure");
-const surfaceButton = document.querySelector(".surface");
 
 hamburger.addEventListener("click", function () {
   document.body.classList.toggle("nav_list_open_body");
   hamburger.classList.toggle("is-active");
 });
 
-overviewButton.classList.add(`${subMenuId}-active`);
+document
+  .querySelector(planetFacts[0].buttonPlanet)
+  .classList.add(`${subMenuId}-active`);
 
 for (const choice of contentButton) {
+  choice.addEventListener("click", function () {
+    choice.classList.add(`${subMenuId}-active`);
+
+    for (const planetFact of planetFacts) {
+      if (
+        document
+          .querySelector(planetFact.buttonPlanet)
+          .classList.contains(`${subMenuId}-active`) &&
+        document.querySelector(planetFact.buttonPlanet) !== choice
+      ) {
+        document
+          .querySelector(planetFact.buttonPlanet)
+          .classList.remove(`${subMenuId}-active`);
+      }
+    }
+  });
+}
+
+/* for (const choice of contentButton) {
   choice.addEventListener("click", function () {
     if (choice.textContent.toLowerCase() === "overview") {
       insidePlanet.classList.remove("visible-planet");
@@ -34,10 +64,12 @@ for (const choice of contentButton) {
       outsidePlanet.classList.remove("visible-planet");
       insidePlanet.classList.add("visible-planet");
       realPlanet.classList.remove("visible-planet");
+
       TxtOutsidePlanet.classList.remove("visible-planet");
       TxtInsidePlanet.classList.add("visible-planet");
       TxtRealPlanet.classList.remove("visible-planet");
       TxtOutsidePlanet.classList.add("invisible-planet");
+
       choice.classList.add(`${subMenuId}-active`);
       overviewButton.classList.remove(`${subMenuId}-active`);
       surfaceButton.classList.remove(`${subMenuId}-active`);
@@ -54,4 +86,4 @@ for (const choice of contentButton) {
       structureButton.classList.remove(`${subMenuId}-active`);
     }
   });
-}
+} */
